e2af235f5bf0b2667f91d94ae82f3785552b4ad8
hadogyun <gkehrbs94@gmail.com>
2025-01-18 13:51:10 +0900

    20250118_BaekJoon
    1 Question

---
+++ Scripts/20250118.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.Scripts
+{
+    //중앙 이동 알고리즘
+    public class _2903번
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            // 1 => 9, 2 => 25, 5 => 1089
+            Console.WriteLine((2 * Math.Pow(2, num - 1) + 1) * (2 * Math.Pow(2, num - 1) + 1));
+        }
+    }
+}
