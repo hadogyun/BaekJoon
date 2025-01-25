ae0c65781adcffacd56d18a2c49ad95f8f543e4f
hadogyun <gkehrbs94@gmail.com>
2025-01-25 11:05:21 +0900

    20250125_BaekJoon
    3 Questions

---
+++ Scripts/20250125.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.Scripts
+{
+    //알고리즘 수업 - 알고리즘의 수행 시간 1
+    public class _24262번
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(1);
+            Console.WriteLine(0);
+        }
+    }
+
+    //알고리즘 수업 - 알고리즘의 수행 시간 2
+    public class _24263번
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(n);
+            Console.WriteLine(1);
+        }
+    }
+
+    //알고리즘 수업 - 알고리즘의 수행 시간 3
+    public class _24264번
+    {
+        static void Main()
+        {
+            ulong n = ulong.Parse(Console.ReadLine());
+
+            Console.WriteLine(n * n);
+            Console.WriteLine(2);
+        }
+    }
+}
