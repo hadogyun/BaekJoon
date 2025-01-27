c38004c9d6a6136ec7862a1fa7b265fd78972add
hadogyun <gkehrbs94@gmail.com>
2025-01-27 14:44:58 +0900

    20250127_BaekJoon
    1 Question

---
+++ Scripts/20250127.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.Scripts
+{
+    //알고리즘 수업 - 알고리즘의 수행 시간 5
+    public class _24266번
+    {
+        static void Main()
+        {
+            long n = long.Parse(Console.ReadLine());
+
+            Console.WriteLine(n * n * n);
+            Console.WriteLine(3);
+        }
+    }
+}
