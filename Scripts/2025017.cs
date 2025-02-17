08a8c1f394faea236c5a393943b5bfdbbdfc7ff0
hadogyun <gkehrbs94@gmail.com>
2025-02-17 11:07:20 +0900

    20250217_BaekJoon
    1 Question

---
+++ Scripts/2025017.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.Scripts
+{
+    //다음 소수
+    public class _4134번
+    {
+        static void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for(long i = 0; i < count; i++)
+            {
+                long num = long.Parse(Console.ReadLine());
+
+                for(long j = num; ; j++)
+                {
+                    if (j <= 1)
+                        continue;
+
+                    if(j <= 3)
+                    {
+                        Console.WriteLine(j);
+                        break;
+                    }
+
+                    if (j % 2 == 0 || j % 3 == 0)
+                        continue;
+
+                    bool isPrime = true;
+
+                    for(long k = 5; k * k <= j; k+=6)
+                    {
+                        if(j % k == 0 || j % (k + 2) == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+
+                    if(isPrime)
+                    {
+                        Console.WriteLine(j);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/20250214.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.Scripts
+{
+    //가로수
+    public class _2485번
+    {
+        static void Main()
+        {
+            StreamReader reader = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter writer = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = int.Parse(reader.ReadLine());
+
+            List<int> numList = new List<int>();
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(reader.ReadLine());
+                numList.Add(num);
+            }
+
+            int gcd = numList[1] - numList[0];
+
+            for (int i = 2; i < n; i++)
+            {
+                int gap = numList[i] - numList[i - 1];
+                gcd = GCD(gcd, gap);
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                int gap = numList[i] - numList[i - 1];
+                count += (gap / gcd) - 1;
+            }
+
+            writer.Write(count);
+
+            reader.Close();
+            writer.Close();
+        }
+
+        static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+    }
+}
