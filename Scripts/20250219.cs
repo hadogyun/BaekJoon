286acb8d310b6c79c3a109450d67151a8ee72f0a
hadogyun <gkehrbs94@gmail.com>
2025-02-19 11:17:20 +0900

    20250219_BaekJoon
    1 Question

---
+++ Scripts/20250219.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.Scripts
+{
+    //베르트랑 공준
+    public class _4948번
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                if (n == 0)
+                    break;
+
+                int count = 0;
+
+                for (int i = n + 1; i <= 2 * n; i++)
+                {
+                    if (IsPrime(i))
+                        count++;
+                }
+
+                Console.WriteLine(count);
+            }
+        }
+
+        static bool IsPrime(int n)
+        {
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
