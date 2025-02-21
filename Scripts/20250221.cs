6c20462734376db0809a12bf0e09b9e905995331
hadogyun <gkehrbs94@gmail.com>
2025-02-21 12:17:05 +0900

    20250221_BaekJoon
    1 Question

---
+++ Scripts/20250221.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekJoon.Scripts
+{
+    //창문 닫기
+    public class _13909번
+    {
+        static void Main()
+        {
+            ulong N = ulong.Parse(Console.ReadLine());
+            int openCount = 0;
+
+            for (ulong i = 1; i <= Math.Sqrt(N); i++)
+                openCount++;
+
+            Console.WriteLine(openCount);
+        }
+    }
+}
